265c4a543b1c1c50b60979bb404b02ba6bc4379f
Mina <Minadaya@gmail.com>
2017-11-18 13:13:21 -0800

    dhappybirthday

---
+++ Javascript/Happybirthday.js
@@ -0,0 +1,21 @@
+var num = 60; 
+{
+    for (var num = 60; num >= 0; num = num - 1){
+        if (num >= 30){
+            console.log(num,"days until my birthday. Such a long time...:(")
+        }
+
+        else if (num > 5){
+            console.log(num,"day until my birthday!")
+        }
+        
+        else if (num >=1){
+            console.log(num,"DAYS UNTIL MY BIRTHDAY!")
+    }
+        else { console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*",
+        "♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪",
+        "*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•");
+            
+        }
+}
+}
