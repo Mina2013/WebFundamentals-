72d257ebfb683b29db522e50a1b6c7cba6d85dcc
Mina <Minadaya@gmail.com>
2017-11-19 17:20:38 -0800

    dbillion

--- Javascript/fancyarray.js
+++ Javascript/fancyarray.js
@@ -1,8 +1,12 @@
-var names = ['james', 'jill','jane','jack'];
-for (var i = 0; i<=arr.lenght; i++){
-     i=i+1;
+var num = 0;
+var names =['james', 'jill','jane','jack'];
+for (var num =0; num <= arr.lenght; num++); 
+{
+  if (num ==6) 
+  {
+    break;
+    // if you have additional code down here, it will never run!
+  }
+  console.log(num, ">", [arr]);
+  num = num + 1;          // if we break, we leave the WHILE loop so these lines won’t run
 }
-
-   console.log(i)
-
-
