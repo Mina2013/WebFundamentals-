c836322085619b4cc02f9f88ba02e26b669dfbe0
Mina <Minadaya@gmail.com>
2017-12-03 19:44:50 -0800

    dtypelist.py

--- Python/typelist.py
+++ Python/typelist.py
@@ -1,20 +1,23 @@
-# def list_type (list):
-#     #print list
-#     for x in list:
-#         #print x
-#         if type (x) == int:
-#             total = 0
-#             for x in list:
-#                 total += x
-#                 return total
-#             print "sum", list_type([1,2,"hello"])
-#         else:
-#             if type (x) == str:
-#                 print "sting:", x
-#         # if isinstance (list, str):
-#         #     print "this is a list of strings"
-#         # else:
-#         #     if isinstance (list, int):
-#         #         print "this is a list of numbers"
-
-#list_type ([1,2,3])
+def list_type (list):
+    #print list
+    total = 0
+    for x in list:
+        #print x
+        if type (x) == int:
+            total+= x
+            #print "this is an integer"
+            print "sum", total
+        else:
+            if type (x) == str:
+                print "sting:", x
+        if isinstance (list, str):
+            print "this is a list of strings"
+        else:
+            if isinstance (list, int):
+                print "this is a list of numbers"
+            else:
+                print "this is a mixed list"
+        # else:
+         #     if isinstance (list, int):
+         #         print "this is a list of numbers"
+list_type ([1,2,3,])
