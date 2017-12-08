a352abce3efc9ef2493a7676a9c5a747be1c9d22
Mina <Minadaya@gmail.com>
2017-12-07 16:15:34 -0800

    dcharacter

---
+++ Python/scores.py
@@ -0,0 +1,21 @@
+import random
+random_num = int
+print "Scores and Grades"
+#def scores(x,y):
+for x in range(3):
+    if random_num >70:
+        print "score:",random.randint(60,70), "the score is D"
+for x in range(3):
+    if random_num >80:
+        print "Score:",random.randint(70,80), "the score is C"
+for x in range(2):
+    if random_num >90:
+        print "Score:",random.randint(80,90), "the score is B"
+for x in range(2):
+    if random_num >100:
+        print "Score:",random.randint(80,90), "the score is A"
+print "Program is done bye"
+
+
+
+#print "Score:", random.randint(60,100)
